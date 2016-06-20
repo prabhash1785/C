2c2b0246f03c07242f8ce7b7fad06e43b7445267
Prabhash Rathore <prabhashrathore@gmail.com>
2016-06-19 22:53:34 -0700

    hello world c program

---
+++ cplayground/cplayground/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cplayground
+//
+//  Created by Rathore, Prabhash on 6/16/16.
+//  Copyright (c) 2016 Rathore, Prabhash. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
